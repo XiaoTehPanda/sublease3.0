32ae128c8bc7a21fb05372d95435d9773b3b3981
Tahamid <sth71392@uga.edu>
2018-11-20 18:50:04 -0500

    official stuff for the intial home page

--- app/src/main/java/edu/uga/csci/xiao/subleased20/IntialPage.java
+++ app/src/main/java/edu/uga/csci/xiao/subleased20/IntialPage.java
@@ -1,18 +1,17 @@
 package edu.uga.csci.xiao.subleased20;
 
-import android.provider.ContactsContract;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
 
-public class MainActivity extends AppCompatActivity {
+public class IntialPage extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.intial_page);
 
         FirebaseDatabase database = FirebaseDatabase.getInstance();
         DatabaseReference myRef = database.getReference("message");
